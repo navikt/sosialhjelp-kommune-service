c299e757e88986ed12ae701a253f7578735416eb
Martin Almvik <martin.almvik@nav.no>
2023-02-10 13:09:39 +0100

    Fjerner CORS plugin

--- src/main/kotlin/no/nav/sosialhjelp/plugins/HTTP.kt
+++ src/main/kotlin/no/nav/sosialhjelp/plugins/HTTP.kt
@@ -2,7 +2,6 @@ package no.nav.sosialhjelp.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.request.path
 import io.ktor.server.response.respondText
@@ -10,16 +9,6 @@ import no.nav.sosialhjelp.NoTokenException
 import no.nav.sosialhjelp.UnauthorizedException
 
 fun Application.configureHTTP() {
-  install(CORS) {
-    allowMethod(HttpMethod.Options)
-    allowMethod(HttpMethod.Get)
-    allowMethod(HttpMethod.Post)
-    allowHeader(HttpHeaders.Authorization)
-    allowHeader(HttpHeaders.XRequestId)
-    allowHeader("Nav-Call-Id")
-    allowHeader("X-XSRF-TOKEN")
-    anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-  }
   install(StatusPages) {
     exception<Throwable> { call, cause ->
       this@configureHTTP.log.error("Error on ${call.request.path()}", cause)
