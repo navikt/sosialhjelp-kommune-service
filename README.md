c227b529eef12a4272a3f94842e4f992231232fe
Martin Almvik <martin.almvik@nav.no>
2023-02-14 15:51:36 +0100

    Gjør repoet lovlig

---
+++ README.md
@@ -0,0 +1,47 @@
+# sosialhjelp-kommune-service
+Ktor GraphQL service som tilbyr informasjon om kommuner
+
+## Henvendelser
+Spørsmål knyttet til koden eller teamet kan stilles til teamdigisos@nav.no.
+
+### For NAV-ansatte
+Interne henvendelser kan sendes via Slack i kanalen #team_digisos.
+
+## Teknologi
+* Kotlin
+* JDK 17
+* Gradle
+* Ktor
+* GraphQL (KGraphQL)
+
+### Krav
+- JDK 17
+
+### Manuell deploy til dev
+Gjøres via GitHub Actions, se: https://github.com/navikt/sosialhjelp-kommune-service/actions/workflows/deploy_dev.yml
+
+## Oppsett av nytt prosjekt
+Dette prosjektet bygger og deployer vha Github Actions
+
+### GitHub package registry
+- Docker image pushes til github package registry, eks [https://github.com/navikt/sosialhjelp-kommune-service/packages/](https://github.com/navikt/sosialhjelp-kommune-service/packages/)
+
+### GitHub Actions
+- Docker image bygges og deployes til alle miljøer ved push til main => `.github/workflows/main.yml`
+- Deploy til dev => `.github/workflows/deploy_dev.yml`
+- Autodeploy til prod-fss fra master => `.github/workflows/deploy_prod.yml`
+
+### GitHub deployment
+- Deployments vises [her](https://github.com/navikt/sosialhjelp-kommune-service/deployments)
+
+## Lokal kjøring
+#### *uten* integrasjon til Fiks, dvs mot mock-alt
+Kjør med miljøvariabel: `ENV=MOCK`. Dette er også default
+#### *med* integrasjon til Fiks
+Kjør med miljøvariabel: `ENV=PROD` eller `ENV=DEV`
+
+Da må følgende env-variabler settes (hentes fra kubernetes secrets): \
+`INTEGRASJONPASSORD_FIKS`, `INTEGRASJONSID_FIKS` og `TESTBRUKER_NATALIE`.
+
+## Hvordan komme i gang
+### [Felles dokumentasjon for våre backend apper](https://github.com/navikt/digisos/blob/main/oppsett-devmiljo.md#backend-gradle)
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 val token_support_version: String by project
 val prometheus_version: String by project
 val kgraphql_version: String by project
+val nimbus_version: String by project
 
 plugins {
   kotlin("jvm") version "1.8.0"
@@ -49,36 +50,28 @@ dependencies {
   implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
   implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
   implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
-  implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
   implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
   implementation("io.ktor:ktor-server-call-id-jvm:$ktor_version")
   implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
   implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-  implementation("io.ktor:ktor-server-openapi:$ktor_version")
-  implementation("io.ktor:ktor-server-swagger:$ktor_version")
   implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-  implementation("io.ktor:ktor-server-resources:$ktor_version")
   implementation("io.ktor:ktor-server-metrics-micrometer:$ktor_version")
-  implementation("io.ktor:ktor-server-status-pages:$ktor_version")
+  implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
 
   // Ktor-client
   implementation("io.ktor:ktor-client-okhttp:$ktor_version")
   implementation("io.ktor:ktor-client-core:$ktor_version")
-  implementation("io.ktor:ktor-client-logging:$ktor_version")
   implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+  implementation("io.ktor:ktor-client-logging:$ktor_version")
 
   implementation("com.apurebase:kgraphql:$kgraphql_version")
   implementation("com.apurebase:kgraphql-ktor:$kgraphql_version")
 
   implementation("ch.qos.logback:logback-classic:$logback_version")
 
-  implementation("no.nav.security:token-validation-core:${token_support_version}")
-  implementation("no.nav.security:token-validation-ktor-v2:${token_support_version}")
+  implementation("com.nimbusds:nimbus-jose-jwt:$nimbus_version")
 
   implementation("io.micrometer:micrometer-registry-prometheus:$prometheus_version")
-  implementation("io.ktor:ktor-client-logging-jvm:2.2.3")
-  implementation("io.ktor:ktor-server-status-pages-jvm:2.2.3")
-  implementation("io.ktor:ktor-client-encoding:2.2.3")
 
   testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
